4e04f13d9b46f44247c1813628069dd3cce8c1ba
karthickow <karthickkrishnan24@gmail.com>
2017-06-25 20:37:06 +0530

    Initial Commit

---
+++ src/main/java/com/learntamil/LearnTamilApplication.java
@@ -0,0 +1,12 @@
+package com.learntamil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTamilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnTamilApplication.class, args);
+	}
+}
