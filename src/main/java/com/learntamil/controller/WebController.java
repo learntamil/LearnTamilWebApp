7a7794c1c98ecce009339bf83e0ac91d87bd8c0d
karthickow <karthickkrishnan24@gmail.com>
2017-07-02 22:09:19 +0530

    Added jpa/hibernate configuration

--- src/main/java/com/learntamil/controller/WebController.java
+++ src/main/java/com/learntamil/controller/WebController.java
@@ -1,14 +1,18 @@
 package com.learntamil.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController 
 {
+	@Value("${learntamil.welcome.page}")
+	private String welcomePage;
+	
 	@GetMapping("/")
 	public String welcome()
 	{
-		return "/html/index.html";
+		return welcomePage;
 	}
 }
