88d25f71faedebd48075851c3e90c518672e1d62
ramasamykasiviswanathan <kramasamy1991@gmail.com>
2017-07-15 19:24:15 +0530

    Angular Modules

---
+++ src/main/resources/static/js/utils.js
@@ -0,0 +1,19 @@
+(() => {
+	/* body... */
+	angular.module('app.utils',[]).
+	factory('Utils', Utils);
+
+	Utils.$inject=["$log"];
+	function Utils($log)
+	{
+		$log.debug("Utils");
+		function isUndefinedOrNull(Obj)
+		{
+			$log.debug("Utils.isUndefinedOrNull");
+			return angular.isUndefined(Obj) || Obj == null;
+		}
+		return {
+			isUndefinedOrNull: isUndefinedOrNull
+		}
+	}
+})()
